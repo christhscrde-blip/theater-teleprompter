16a89376a2ef8749078514fc59e7e480a17f993c
ChrisBlip <christoph.schumacher.de@gmail.com>
2026-07-21 12:50:37 +0200

    Fix script loading on Safari and iPad

--- script-data.js
+++ script-data.js
@@ -1,7 +1,21 @@
 window.loadTheaterScript = async function () {
-  const binary = atob(window.THEATER_DATA_CHUNKS.join(''));
-  const bytes = Uint8Array.from(binary, char => char.charCodeAt(0));
-  const stream = new Blob([bytes]).stream().pipeThrough(new DecompressionStream('gzip'));
-  const text = await new Response(stream).text();
+  const encoded = window.THEATER_DATA_CHUNKS.join('');
+  const binary = atob(encoded);
+  const bytes = new Uint8Array(binary.length);
+  for (let index = 0; index < binary.length; index += 1) {
+    bytes[index] = binary.charCodeAt(index);
+  }
+
+  let text;
+  if (typeof TextDecoder !== 'undefined') {
+    text = new TextDecoder('utf-8').decode(bytes);
+  } else {
+    let escaped = '';
+    for (let index = 0; index < bytes.length; index += 1) {
+      escaped += `%${bytes[index].toString(16).padStart(2, '0')}`;
+    }
+    text = decodeURIComponent(escaped);
+  }
+
   return JSON.parse(text);
 };
